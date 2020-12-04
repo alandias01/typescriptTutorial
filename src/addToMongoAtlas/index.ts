1345b187cd0068b13c64837beabd18b9d79cfed4
alan <alandias01@yahoo.com>
2020-12-03 23:53:04 -0500

    ReadFilegreList

--- src/addToMongoAtlas/index.ts
+++ src/addToMongoAtlas/index.ts
@@ -1,45 +1,66 @@
 import mongoose, { Mongoose, Schema } from "mongoose";
 const fs = require("fs");
 const csv = require("csv-parser");
+import {
+  AST,
+  InsertReplaceValue,
+  Insert_Replace,
+  Parser,
+} from "node-sql-parser";
 
-const stockSchema = new Schema({
-  Symbol: { type: String },
-  Name: { type: String },
-  Sector: { type: String },
-  Price: { type: String },
-  "Price/Earnings": { type: String },
-  "Dividend Yield": { type: String },
-  "Earnings/Share": { type: String },
-  "52 Week Low": { type: String },
-  "52 Week High": { type: String },
-  "Market Cap": { type: String },
-  EBITDA: { type: String },
-  "Price/Sales": { type: String },
-  "Price/Book": { type: String },
-  "SEC Filings": { type: String },
+const user = process.env.MONGOATLAS_USER;
+const pw = process.env.MONGOATLAS_PASSWORD;
+const db = process.env.MONGOATLAS_DB;
+const mongoAtlasConnectionString = `mongodb+srv://${user}:${pw}@cluster0-yznr0.mongodb.net/${db}?retryWrites=true&w=majority`;
+
+const connOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongoAtlasConnection = mongoose.createConnection(
+  mongoAtlasConnectionString,
+  connOptions
+);
+
+mongoAtlasConnection.catch((error) => {
+  console.log("mongoAtlasConnection error");
+  console.log(error);
 });
 
-const stockModel = mongoose.model("stock", stockSchema);
+/************************** START *******************************/
 
-mongoose
-  .connect(
-    "mongodb+srv://Root:<password>@cluster0-yznr0.mongodb.net/portfolio?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(Start)
-  .catch((err) => {
-    console.log(err);
-  });
+ReadFilegreList(mongoAtlasConnection);
 
-function Start(connectionDetails: Mongoose) {
-  const instance = new stockModel();
-  ReadFile();
+/************************** START END****************************/
+
+function testfunc() {
+  try {
+    console.log("entered testfunc properly");
+  } catch (error) {
+    console.log("Error in testfunc");
+    console.log(error);
+  }
 }
 
-function ReadFile() {
+/************************** Use for Stock************************/
+
+function InsertStockPrices() {
+  const stockSchema = new Schema({
+    Symbol: { type: String },
+    Name: { type: String },
+    Sector: { type: String },
+    Price: { type: String },
+    "Price/Earnings": { type: String },
+    "Dividend Yield": { type: String },
+    "Earnings/Share": { type: String },
+    "52 Week Low": { type: String },
+    "52 Week High": { type: String },
+    "Market Cap": { type: String },
+    EBITDA: { type: String },
+    "Price/Sales": { type: String },
+    "Price/Book": { type: String },
+    "SEC Filings": { type: String },
+  });
+
+  const stockModel = mongoose.model("stock", stockSchema);
+
   fs.createReadStream("./constituents-financials.csv")
     .pipe(csv())
     .on("data", async (data: any) => {
@@ -54,23 +75,93 @@ function ReadFile() {
       console.log("done");
     });
 }
+/************************** Use for Stock END********************/
 
 /************************** Use for gre**************************/
-const greSchema = new Schema({
-  Word: { type: String },
-  Definition: { type: String },
-  Type: { type: String },
-});
 
-const greModel = mongoose.model("gre", greSchema);
-
-const ReadFilegre = () => {
-  console.log("object");
-  let rawdata = fs.readFileSync("grewords.json");
+function ReadFilegre() {
+  let rawdata = fs.readFileSync("./src/addToMongoAtlas/grewords.json");
   let greParsed = JSON.parse(rawdata);
   greParsed.forEach((x: any) => {
     console.log(x);
   });
-};
+
+  const greSchema = new Schema({
+    Word: { type: String },
+    Definition: { type: String },
+    Type: { type: String },
+  });
+
+  const greModel = mongoose.model("gre", greSchema);
+}
+
+function ReadFilegreList(mongoConn: mongoose.Connection) {
+  try {
+    const fileUrl = "./src/addToMongoAtlas/grelist.sql";
+    const rawdata: string = fs.readFileSync(fileUrl, "utf8");
+
+    const parser = new Parser();
+    const [astFirst] = parser.astify(rawdata) as AST[];
+    const ast = astFirst as Insert_Replace;
+
+    // console.log(ast);
+    // ast.values.forEach((x) => {
+    //   console.log(x);
+    //   x.value.forEach((listItem: InsertReplaceValue) => {
+    //     console.log(listItem.value);
+    //   });
+    // });
+
+    const greListSchema = new Schema({
+      email: { type: String },
+      list: { type: String },
+      word: { type: String },
+    });
+
+    const greListModel = mongoConn.model("lists", greListSchema);
+
+    let promiseList: Promise<mongoose.Document>[] = [];
+    ast.values.forEach((x) => {
+      const listObject: InsertReplaceValue[] = x.value;
+
+      const [email, list, word] = listObject;
+      //console.log(`email:${email.value} list:${list.value} word:${word.value}`);
+      const data = { email: email.value, list: list.value, word: word.value };
+      const instance = new greListModel(data);
+      promiseList.push(instance.save());
+    });
+
+    Promise.all(promiseList)
+      .then((x) => {
+        console.log("All data inserted");
+      })
+      .catch((error) => {
+        console.log("Error inserting data");
+      });
+  } catch (error) {
+    console.log("Error in ReadFilegreList");
+    console.log(error);
+  }
+}
+
+//We have an sql insert statement for list items that we want to deconstruct into objects
+function parsesql() {
+  try {
+    const sql =
+      "INSERT INTO lists (email, list, word) VALUES ('a@a.com','frankenstein','abhorrence'),('a@a.com','frankenstein','abode')";
+    const parser = new Parser();
+    const ast = parser.astify(sql) as Insert_Replace;
+    console.log(ast);
+    ast.values.forEach((x: InsertReplaceValue) => {
+      console.log(x.value);
+      x.value.forEach((listItem: InsertReplaceValue) => {
+        console.log(listItem.value);
+      });
+    });
+  } catch (error) {
+    console.log("There was an error");
+    console.log(error);
+  }
+}
 
 /************************** Use for gre END**********************/
