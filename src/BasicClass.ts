ea7ecfbce5083cd58832fc1bf10b35cccba009c0
alan <alandias01@yahoo.com>
2021-02-10 14:29:08 -0500

    update

--- src/BasicClass.ts
+++ src/BasicClass.ts
@@ -1,32 +1,26 @@
 //**********VARIABLES**********
 
-let num = 5; //num = "a";  //throws compile error since type is different but will still transpile to js
-
 //type is any
 let anyval;
 anyval = 5;
 anyval = "a";
 
-let msg = "f"; //msg.endsWith('c');  es6
-
 //if type any, you won't get intellisense
 let msg1; //msg1. no intellisense
 
 //to get intellisense from variable of type any, use type assertions.
-//let endsWithC = (<string>msg1).endsWith('c');
-//let endsWithC2 = (msg1 as string).endsWith('c');
+let endsWithC = (<string>(<unknown>msg1)).endsWith("c");
+let endsWithC2 = ((msg1 as unknown) as string).endsWith("c");
 
-//How to set type annotations
+//Setting the type is referred to as setting the type annotations
 let vAnnotations0: any;
 let vAnnotations1: number;
 let vAnnotations2: string;
 let vAnnotations3: boolean;
-
+let vAnnotations4: { name: string; age: number } = { name: "Alan", age: 1 };
 let vAnnotations5: number[];
 let vAnnotations6: number[] = [1, 2, 3];
 let vAnnotations7: any[] = [1, "a", 3];
-
-const ColorRed = 0;
 
 enum Color {
   Red = 1,
@@ -37,19 +31,38 @@ let bgColor = Color.Red; //intellisense support
 
 //**********Functions**********
 
-//Functions js way
-var log = function (msg) {
+//Named Function.  msg is a parameter.  When someone uses the function and passes in a value, it
+//is referred to as an argument
+function namedFunc(msg: string) {
   console.log(msg);
+  return msg + " added";
+}
+
+//Anonymous Function
+var anonymousFunc = function (msg: string) {
+  console.log(msg);
+  return msg + " added";
 };
 
-//typescript
-//2nd arg is an optional arg;  3rd arg is optional with default val
+//Arrow Function
+const arrowFunc1 = (msg: string) => {
+  console.log(msg);
+  return msg + " added";
+};
+
+//Arrow Function that only returns something with no body
+const arrowFunc2 = (msg: string) => msg + " added";
+
+//Optional and default arguments
+//2nd arg is an optional arg;
+//3rd arg is optional with default val
 //If default args are placed before required args, then you must pass undefined
-let log1 = function (msg: string, msg2?: string, msg3 = "defaultVal") {
+let log1 = (msg: string, msg2?: string, msg3 = "defaultVal") => {
   console.log(msg);
 };
 
 //A more in depth example, take the below.  This is the fully typed function
+//Here we say myAdd is a function with this signature (x: number, y: number) => number
 let myAdd: (x: number, y: number) => number = function (
   x: number,
   y: number
@@ -66,6 +79,20 @@ function myAdd3(x: number, y: number): number {
   return x + y;
 }
 
+//You can use an interface and a type
+interface IMyAdd {
+  x: number;
+  y: number;
+}
+
+type IReturnMyAdd = (numbers: IMyAdd) => number;
+
+let myAdd4: IReturnMyAdd = (numbers) => numbers.x + numbers.y;
+
+//without interface
+let myAdd5 = (numbers: { x: number; y: number }): number =>
+  numbers.x + numbers.y;
+
 //passing callbacks
 function fn(x: () => void) {
   x();
@@ -73,25 +100,6 @@ function fn(x: () => void) {
 fn(() => {
   console.log("call back func");
 });
-
-//arrow functions (c# lambda)
-let log2 = (msg: string) => {
-  console.log(msg);
-};
-let log3 = (msg) => console.log(msg);
-let log4 = () => console.log();
-
-//instead of passing many args
-//let drawPoint = (a,b,c,d,e,f,g) => { }  pass in an object
-
-let drawPoint = (point) => {};
-
-drawPoint({ x: 1, y: 2 });
-
-//problem is drawpoint can then be changed to
-//drawPoint({    name: "Alan" })
-//To protect against that, a verbose way, inline annotations
-let drawPoint2 = (point: { x: number; y: number }) => {};
 
 //SWITCH
 function switchstmt(grade: string) {
@@ -111,16 +119,13 @@ function switchstmt(grade: string) {
   }
 }
 
-//**********INTERFACES**********
+//**********Class**********
 
 interface IBasicClass {
   x: number;
   y: number;
   drawPoint: () => void;
 }
-
-let drawPoint3 = (point: IBasicClass) => {};
-let getDistance = (pointA: IBasicClass, pointB: IBasicClass) => {};
 
 export class BasicClass implements IBasicClass {
   //x: number; Already defined in constructor feature
